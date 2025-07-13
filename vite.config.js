daeacbadca0a0ba0e977379313e6b8eabcf87d37
Pratham <prathamjuvekar20@gmail.com>
2025-07-13 23:16:57 +0530

    Fix: vite config and prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://prathamjuvekar.github.io/EasyEats/', // ✅ This is the fix
   plugins: [react()],
-})
+});
