c00c4c90d697e7ac90108f730031a8ae7ad8cdf4
Pratham <prathamjuvekar20@gmail.com>
2025-07-13 23:31:03 +0530

    Fix: added basename to BrowserRouter for GitHub Pages LAST ONE

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import StoreContextProvider from './Context/StoreContext';
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './Context/StoreContext'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/EasyEats">
