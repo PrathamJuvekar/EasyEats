920e088985ce488ea9cfdbf877d4e11e2d209fcb
Pratham <prathamjuvekar20@gmail.com>
2025-07-13 23:21:18 +0530

    Fix Vite base path for GitHub Page 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://prathamjuvekar.github.io/EasyEats/', // ✅ This is the fix
   plugins: [react()],
+  base: '/EasyEats/', 
 });
